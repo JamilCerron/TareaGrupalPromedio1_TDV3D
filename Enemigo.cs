55d14745054d4308653f10c08eec4181f58c5894
JamilCerron <luis.cerron.2010@gmail.com>
2024-09-26 22:58:48 -0500

    Crear estructura de clases Menu y Enemigo según diagrama de clases
    Se implementaron las clases basadas en el diagrama, incluyendo sus atributos y métodos.

---
+++ Enemigo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaGrupalPromedio1_TDV3D_Progra
+{
+    internal class Enemigo
+    {
+        public string nombre;
+        public int vida;
+
+        Enemigo(string nombre, int vida)
+        {
+            this.nombre = nombre;
+            this.vida = vida;
+        }
+
+        public void EstaVivo()
+        {
+
+        }
+        
+    }
+}
---
+++ Menu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaGrupalPromedio1_TDV3D_Progra
+{
+    internal class Menu
+    {
+        //public List<Estructura>();
+        //public List<Enemigos>();
+        public int dinero;
+        public int fibonacciIndex;
+        public int turnosSobrevividos;
+        public  void EjecutarMenu()
+        {
+
+        }
+
+        public void MetodoFibonacci()
+        {
+
+        }
+
+        public void VerEnemigos()
+        {
+
+        }
+
+        public void TurnoDelEnemigo()
+        {
+
+        }
+    }
+}
